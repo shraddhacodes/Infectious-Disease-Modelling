f1ee320fc963a366569e2520e63e7bba4d165f80
Shraddha Kodavade <102799563+shraddhacodes@users.noreply.github.com>
2023-12-27 10:45:37 +0530

    Update Discrete Renewal Model.R
    Added Codes to the R script to run Stan Model as well.

--- Discrete Renewal Model.R
+++ Discrete Renewal Model.R
@@ -1,4 +1,14 @@
 #Discrete Renewal Equation Modelling
+#Part 1: R
+
+# pop_size : Population size.
+# I.init : Initial number of infectious individuals.
+# R0 : Basic reproduction number.
+# GI_span : Maximum value for GI
+# GI_mean : Mean for GI
+# GI_var : Variance for GI
+# GI_type : Type of distribution for the GI. 'pois' or 'nbinom'
+# horizon : Time horizon of the simulation.
 
 gi.distrib <- function(type, mean, var, span, x) {
   res <- NULL
@@ -9,15 +19,6 @@ gi.distrib <- function(type, mean, var, span, x) {
   }
   return(res)
 }
-
-# pop_size : Population size.
-# I.init : Initial number of infectious individuals.
-# R0 : Basic reproduction number.
-# GI_span : Maximum value for GI
-# GI_mean : Mean for GI
-# GI_var : Variance for GI
-# GI_type : Type of distribution for the GI. 'pois' or 'nbinom'
-# horizon : Time horizon of the simulation.
 
 simulate <- function(pop_size, I.init, R0, GI_span, GI_mean, GI_var, GI_type, horizon) {
   if (length(I.init) == 1) {
@@ -40,3 +41,32 @@ simulate <- function(pop_size, I.init, R0, GI_span, GI_mean, GI_var, GI_type, ho
   }
   return(list(S = S, I = I))
 }
+simulate(1000,1,4.513,100,30,10,'pois',10)
+
+#Part 2: Stan
+
+setwd("C:/Users/hp/OneDrive/Desktop/Shraddha laptop backup/Sumali Mam/codes")
+
+install.packages("rstan")
+library(rstan)
+
+# Compile the Stan model
+stan_model_code <- stan_model('Discrete Renewal Model Stan.stan')
+
+# Data for the Stan model
+data_list <- list(
+  pop_size = 1000,
+  I_init = 10,
+  R0 = 2,
+  GI_span = 5,
+  GI_mean = 2,
+  GI_var = 2,
+  GI_type = 0,  # 0 for 'pois', 1 for 'nbinom'
+  horizon = 50
+)
+
+# Run the Stan model
+stan_output <- sampling(stan_model_code, data = data_list, chains = 4, iter = 1000, warmup = 500)
+
+# Print a summary of the Stan output
+print(stan_output)
